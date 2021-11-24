0f95e6ff5725380819a9e63704a09ddc359b59ea
Shaun Wilde <shaun.wilde@estimateone.com>
2021-11-24 21:22:06 +1100

    feat: allow ghost articles, meeting the criteria, to be syndicated to Medium

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-** NOT READY **
-
 [![CI](https://github.com/Many-Monkeys/gatsby-plugin-ghost-syndicate-medium/actions/workflows/main.yml/badge.svg)](https://github.com/Many-Monkeys/gatsby-plugin-ghost-syndicate-medium/actions/workflows/main.yml)
 # gatsby-plugin-ghost-syndicate-medium
 Syndicate blog articles to Medium from your gatsby/ghost blog
+
+## How to use this plugin
+1. install the package eg `npm i @many-monkeys/gatsby-plugin-ghost-syndicate-medium`
+2. add the following to your `gatsby-config.js`
+
+```js
+        {
+            resolve: `@many-monkeys/gatsby-plugin-ghost-syndicate-medium`,
+            options: {
+                apiToken: `your-medium-integration-token`,  // see https://medium.com/me/settings
+                syndicationTag: `#medium`,                  // a tag (public or internal) to control access to which articles are syndicated - default null means all articles are considered
+                age: 300,                                   // how recent an article (updated) should be to be considered for syndication, between 180 and 3600 seconds, default 300
+                continueOnFailure: false,                    // allow publish to continue if failure occurs during syndication, default true
+            },
+        },
+```
+
+## Caveats
+
+1. Articles are only published in draft mode, you will need to login into medium to complete the publish
+2. There is no API ([medium](https://github.com/Medium/medium-api-docs)) to list published articles or to update existing ones and so it is possible (probable) that you'll have multiple articles with the same name; this is why all articles are currently published as draft (*).
+3. Internal tags (those beginning with #) are removed from the post
+4. Medium has rate limiting in place - this is not yet handled as it is rare that more than a few articles would be updated at a time.
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,74 @@
+const axios = require('axios');
+const { DateTime } = require('luxon');
+
+const syndicationQuery = `
+{
+  pages: allGhostPost {
+    edges {
+      node {
+        slug
+        title
+        updated_at
+        url
+        published_at
+        html
+        tags {
+          name
+          visibility
+        }
+        visibility
+        feature_image
+      }
+    }
+  }
+}`
+
+function pageToMediumRecord({ node: { id, title, html, tags, url, feature_image, ...rest } }) {
+  const visibleTags = tags
+    .filter(tag => tag.visibility === 'public')
+    .map(tag => tag.name);
+
+  const allTags = tags
+    .map(tag => tag.name);
+
+  return {
+    medium: {
+      title,
+      contentFormat: 'html',
+      publishStatus: 'draft',
+      content: `<img src="${feature_image}" alt="${title}"/>${html}`,
+      tags: visibleTags,
+      canonicalUrl: url
+    },
+    tags: allTags,
+    ...rest,
+  }
+}
+
+const queries = {
+  query: syndicationQuery,
+  transformer: ({ data }) => data.pages.edges.map(pageToMediumRecord)
+}
+
+const getMyProfile = async (apiToken) => {
+  const res = await axios.get('https://api.medium.com/v1/me', {
+    headers: { 'Authorization': 'Bearer ' + apiToken}
+  });
+  return res.data;
+}
+
+const postMediumArticle = async (apiToken, mediumUserId, mediumData) => {
+  const res = await axios.post(`https://api.medium.com/v1/users/${mediumUserId}/posts`, mediumData, {
+    headers: { 'Authorization': 'Bearer ' + apiToken}
+  });
+  return res.data;
+}
+
 exports.onPostBuild = async function ({ graphql, reporter }, config) {
   const {
     apiToken,
+    syndicationTag,
+    age = 300,
     continueOnFailure = true,
   } = config;
 
@@ -13,6 +81,35 @@ exports.onPostBuild = async function ({ graphql, reporter }, config) {
     );
     activity.end();
     return;
+  }
+
+  try {
+    const boundAge = Math.max(Math.min(age, 3600), 180);
+    const sinceUTC = DateTime.now().minus({ seconds: boundAge }).toUTC();
+
+    const result = await graphql(queries.query);
+    const syndicatedPosts = (await queries.transformer(result))
+      .filter(post => post.visibility === 'public')
+      .filter(post => !syndicationTag || post.tags.includes(syndicationTag))
+      .filter(post => DateTime.fromISO(post.updated_at).toUTC() > sinceUTC)
+      .sort((a, b) => b.published_at.localeCompare(a.published_at));
+
+    if (syndicatedPosts.length > 0) {
+      const mediumUser = await getMyProfile(apiToken);
+      for (let index = 0; index < syndicatedPosts.length; index++) {
+        const post = syndicatedPosts[index];
+        const res = await postMediumArticle(apiToken, mediumUser.data.id, post.medium);
+        console.log(res.data);
+      }
+    }
+
+    } catch (error) {
+      if (continueOnFailure) {
+        reporter.warn('failed to syndicate to Medium');
+        console.error(error);
+      } else {
+        activity.panicOnBuild('failed to syndicate to Medium', error);
+      }
   }
 
   activity.end();
